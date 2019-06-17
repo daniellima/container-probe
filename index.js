8672d5ef4240060455415c9f9e489d59afdcaeb4
Daniel Lima <daniel.santos@stone.com.br>
2019-06-17 11:53:48 -0300

    Change healthz to ping and create a second healtcheck
    healthz appears to be taken by NGINX by default. We must test better.
    
    The second healthcheck will help when testing with GLB. We want to see
    that the GLB healthcheck updates when the container healthcheck changes.

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const os = require('os')
 const app = express()
 const port = 3000
 
-var response_status = 200
+var ping_reponse = 200
+var pong_reponse = 200
 
 app.get('/', (req, res) => {
     return res.send('Hello World! I am listening...');
 })
 
-app.get('/healthz', (req, res) => {
-    console.log(`${os.hostname()}: Received request for /healthz endpoint at ${new Date()}`);
-    return res.status(response_status).send(`Responding with ${response_status}`);
+app.get('/ping', (req, res) => {
+    console.log(`${os.hostname()}: Received request for /ping endpoint at ${new Date()}`);
+    return res.status(ping_reponse).send(`Responding ping with ${ping_reponse}`);
 })
 
-app.get('/change_response', (req, res) => {
+app.get('/pong', (req, res) => {
+    console.log(`${os.hostname()}: Received request for /pong endpoint at ${new Date()}`);
+    return res.status(pong_reponse).send(`Responding pong with ${pong_reponse}`);
+})
+
+app.get('/change_ping', (req, res) => {
     desired_respose_status = req.query.status
     if (desired_respose_status === undefined || desired_respose_status === "") {
         return res.status(400).send("Parameter 'status' not send.");
@@ -23,8 +29,22 @@ app.get('/change_response', (req, res) => {
         return res.status(400).send(`Invalid value for 'status' parameter. Must be numeric like '200' or '400'. Received '${desired_respose_status}'`);
     }
 
-    res.send(`Changing response from ${response_status} to ${req.query.status}`);
-    response_status = req.query.status;
+    res.send(`Changing response from ${ping_reponse} to ${req.query.status}`);
+    ping_reponse = req.query.status;
+    return res;
+})
+
+app.get('/change_pong', (req, res) => {
+    desired_respose_status = req.query.status
+    if (desired_respose_status === undefined || desired_respose_status === "") {
+        return res.status(400).send("Parameter 'status' not send.");
+    }
+    if (isNaN(desired_respose_status)) {
+        return res.status(400).send(`Invalid value for 'status' parameter. Must be numeric like '200' or '400'. Received '${desired_respose_status}'`);
+    }
+
+    res.send(`Changing response from ${pong_reponse} to ${req.query.status}`);
+    pong_reponse = req.query.status;
     return res;
 })
 
